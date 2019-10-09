196c7573cda43e6cf31998a33a40a31356cd8e89
Ta-tiana <ustinya88@gmail.com>
2019-10-09 13:38:21 +0300

    feat: completed tic-tac-toe task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,97 @@
 class TicTacToe {
     constructor() {
-
+        this.player = "x";
+        this.finished = false;
+        this.draw = false;
+        this.noTurns = false;
+        this.winner = null;
+        this.matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
+        ]
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.player;
     }
 
     nextTurn(rowIndex, columnIndex) {
+        let temp = this.matrix[rowIndex][columnIndex];
 
+        if (temp !== "x" && temp !== "o") {
+            for (let i = 0; i < this.matrix.length; i++) {
+                for (let j = 0; j < this.matrix[i].length; j++) {
+                    if (this.matrix[i][j] === temp) {
+                        this.matrix[i][j] = this.player;
+                    }
+                }
+            }
+            if (this.player === "x") {
+                this.player = "o"
+            } else {
+                this.player = "x";
+            }
+        }
     }
 
     isFinished() {
-
+        this.noMoreTurns();
+        for (let i = 0; i < this.matrix.length; i++) {
+            const temp = this.matrix[i];
+            if (temp[0] === temp[1] && temp[1] === temp[2] || this.noTurns) {
+                this.finished = true;
+                return this.finished;
+            }
+        }
+        return this.finished;
     }
 
     getWinner() {
-
+        for (let i = 0; i < this.matrix.length; i++) {
+            const temp = this.matrix[i];
+            if (temp[0] === temp[1] && temp[1] === temp[2]) {
+                this.winner = temp[0];
+                return this.winner;
+            }
+        }
+        return this.winner;
     }
 
     noMoreTurns() {
-
+        for (let i = 0; i < 3; i++) {
+            let temp = this.matrix[i];
+            for (let j = 0; j < 3; j++) {
+                if (temp[j] !== "x" && temp[j] !== "o") {
+                    this.noTurns = false;
+                    return this.noTurns;
+                }
+            }
+        }
+        this.noTurns = true;
+        return this.noTurns;
     }
 
     isDraw() {
 
+        this.noMoreTurns(); this.getWinner();
+        if (this.noTurns && this.winner === null) {
+            this.draw = true;
+        }
+        return this.draw;
     }
 
     getFieldValue(rowIndex, colIndex) {
+        let temp = this.matrix[rowIndex][colIndex];
 
+        if (temp !== "x" && temp !== "o") {
+            return null;
+        } else return temp;
     }
 }
 
